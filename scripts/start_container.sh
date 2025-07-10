c25b5bd9dee51abcd6fdfe02261ac201b26e5998
jay vadadoriya <145242208+jayvadadoriya@users.noreply.github.com>
2025-07-10 15:38:10 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@
   docker pull trainwithshubham/tws-online-shop-app-demo:latest
 
   # Run the Docker image as a container
-  docker run -d -p 5173:5173 trainwithshubham/tws-online-shop-app-demo:latest
+  docker run -d -p 5173:5173 jayvadadoriya/tws-online-shop-app-demo:latest
